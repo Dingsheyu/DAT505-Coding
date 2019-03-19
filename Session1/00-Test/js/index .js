9fc464256c68729f1124491ab9daf3f3172d0b0d
Dingsheyu <1473914417@qq.com>
2019-03-19 09:55:36 +0800

    hello

---
+++ Session1/00-Test/js/index .js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="UFO-8" />
+     <title>Three.js</title>
+      <style>
+       body {margin: 0; }
+       canvas {width: 100%;height: 100% }
+     </style>
+   <script src="build/three.js"></script>
+ </head>
+ <body>
+ </body>
+</html>
